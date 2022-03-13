fb30706460b2ef0a83459e769c9baab56326d50a
jjpp43 <junnapark@gmail.com>
2022-03-13 06:43:24 +0000

    Basic cookie insertion

--- chapter4/cookiesAndSessions/cookie.js
+++ chapter4/cookiesAndSessions/cookie.js
@@ -6,4 +6,8 @@ http.createServer((req, res) => {
     res.end('Hello Cookie');
 }).listen(8083, () => {
     console.log('8083번 포트에서 서버 대기 중...');
-})
+});
+
+//쿠키는 req.headers.cookie에 들어있음. 
+//쿠키는 요청과 응답의 헤더를 통해 오고가기 떄문에 응답의 헤더에 쿠키를 기록하기 위해서 res.writeHead메서드를 사용함.
+//Set-Cookie : tells the browser to store the cookie (in this case : mycookie=test)
